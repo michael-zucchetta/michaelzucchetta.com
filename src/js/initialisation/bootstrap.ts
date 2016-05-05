c36016446ccc6097325067d37dba839343f41bd3
michael-zucchetta <michael.zucchetta@gmail.com>
2016-05-05 23:18:34 +0000

    Configured requirejs for main

---
+++ src/js/initialisation/bootstrap.ts
@@ -0,0 +1,46 @@
+requirejs.config({
+	baseUrl: '/',
+
+	paths: {
+		"angular": "../lib/angularjs/angular.min",
+		"angularRoute": "../lib/angular-route/angular-route.min",
+		"angularResource": "../lib/angular-resource/angular-resource.min",
+		"angularCss": "../lib/angular-css/angular-css.min",
+		"jQuery": "../lib/jquery/dist/jquery.min",
+		"lodash": "../lib/lodash/lodash.min",
+		"main": "js/initialisation/main"
+	},
+
+	shim: {
+		'angular' : {
+			exports : 'angular'
+		},
+		'angularRoute': {
+			deps: ['angular'],
+			exports: 'angularRoute'
+		},
+		'angularMocks': {
+			deps: ['angular'],
+			exports: 'angularMocks'
+		},
+		'angularResource': {
+			deps: ['angular'],
+			exports: 'angularResource'
+		},
+		'angularCss': {	
+			deps: ['angular'],   
+			exports: 'angularCss'	
+		},
+		'main': {
+			deps: ['angular', 'angularRoute', 'angularCss'],
+			exports: 'main'
+		}
+	}
+});
+
+requirejs([
+		'main'
+	],
+	(app) => {
+
+});
--- src/js/initialisation/main.ts
+++ src/js/initialisation/main.ts
@@ -1,58 +1,59 @@
-(() => {
-	let module = angular.module('michaelzucchetta', ['ngRoute', 'angularCSS']);
-	class AngularBootstrap implements ng.IAngularBootstrapConfig {
-		constructor($routeProvider, $locationProvider, $controllerProvider, $provide, $compileProvider, $componentProvider) {
-			let app: ng.IModule = module;
-			// http://www.bennadel.com/blog/2554-loading-angularjs-components-with-requirejs-after-application-bootstrap.htm
-			angular.extend(app, {
-				// keep the older references.
-				_controller: app.controller,
-				_service: app.service,
-				_factory: app.factory,
-				_value: app.value,
-				_directive: app.directive,
-				_component: app.component,
+import angular = require('angular');
 
-				controller: (name: string, constructorController: Function) => {
-					$controllerProvider.register(name, constructorController);
-					return app;
-				},
+let module = angular.module('michaelzucchetta', ['ngRoute', 'angularCSS']);
+module.run([() => {
+}]);
 
-				service: (name: string, constructor: Function) => {
-                                	$provide.service(name, constructor);
-                                	return app;
-				},
+class AngularBootstrap implements ng.IAngularBootstrapConfig {
+	constructor($routeProvider, $locationProvider, $controllerProvider, $provide, $compileProvider) {
+		let app: ng.IModule = module;
+		// http://www.bennadel.com/blog/2554-loading-angularjs-components-with-requirejs-after-application-bootstrap.htm
+		angular.extend(app, {
+			// keep the older references.
+			_controller: app.controller,
+			_service: app.service,
+			_factory: app.factory,
+			_value: app.value,
+			_directive: app.directive,
+			_component: app.component,
 
-				factory: (name: string, factory: any[]) => {
-                                	$provide.factory(name, factory);
-                                	return app;
-				},
-				
-				value: (name: string, value: any) => {
-                                	$provide.value(name, value);
-                                	return app;
-				},
+			controller: (name: string, constructorController: Function) => {
+				$controllerProvider.register(name, constructorController);
+				return app;
+			},
 
-        	                directive: (name: string, factory: ng.IDirectiveFactory) => {
-                                	$compileProvider.directive(name, factory);
-                                	return app;
-                        	},
+			service: (name: string, constructor: Function) => {
+				$provide.service(name, constructor);
+				return app;
+			},
 
-                        	component: (name: string, options: ng.IComponentOptions) => {
-                                	$componentProvider.component(name, options);
-                                	return app;
-                        	}
-			});
-                        // NOTE: You can do the same thing with the "filter"
-                        // and the "$filterProvider"; but, I don't really use
-                        // custom filters.
-		}
-	};
-	
-	AngularBootstrap.$inject = ['$routeProvider', '$locationProvider', '$controllerProvider', '$provide', '$compileProvider', '$componentProvider'];
+			factory: (name: string, factory: any[]) => {
+				$provide.factory(name, factory);
+				return app;
+			},
+			
+			value: (name: string, value: any) => {
+				$provide.value(name, value);
+				return app;
+			},
 
-	module.config(AngularBootstrap);
+			directive: (name: string, factory: ng.IDirectiveFactory) => {
+				$compileProvider.directive(name, factory);
+				return app;
+			},
 
-	module.run([() => {
-	}]);
-})();
+			component: (name: string, options: ng.IComponentOptions) => {
+				$compileProvider.component(name, options);
+				return app;
+			}
+		});
+		// NOTE: You can do the same thing with the "filter"
+		// and the "$filterProvider"; but, I don't really use
+		// custom filters.
+	}
+};
+
+AngularBootstrap.$inject = ['$routeProvider', '$locationProvider', '$controllerProvider', '$provide', '$compileProvider'];
+
+module.config(AngularBootstrap);
+angular.element().ready(() => angular.bootstrap(document, ['michaelzucchetta']));
