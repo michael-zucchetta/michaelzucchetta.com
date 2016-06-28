674e348925109609cbd4e1f514f58dd4361cee5d
Michael Zucchetta <michael.zucchetta@gmail.com>
2016-06-28 20:22:01 +0100

    animation finished

--- src/components/global/global.ctrl.ts
+++ src/components/global/global.ctrl.ts
@@ -39,13 +39,16 @@ export default class GlobalCtrl {
 		var body: any = document.querySelector('body');
 		body.style.visibility = 'visible';
 
-		let line1: any = document.querySelector('.line1 .terminal-text');
+		let line1: any = document.querySelector('.line1');
+		let line1Text: any = document.querySelector('.line1 .terminal-text');
 		let line1Cursor: any = document.querySelector('.line1 .white-cursor');
 		let websiteContent: any = document.querySelector('.website-content');
-		websiteContent.style.animationPlayState = 'running';
+    websiteContent.style.animationPlayState = 'running';
+		websiteContent.style.webkitAnimationPlayState = 'running';
 		websiteContent.addEventListener('animationend', () => {
 			setTimeout(() => {
-				line1.style.animationPlayState = 'running';
+				line1Text.style.animationPlayState = 'running';
+				line1Text.style.webkitAnimationPlayState = 'running';
 			}, 200);
 		});
 
@@ -72,11 +75,48 @@ export default class GlobalCtrl {
 
 		let animation: any = document.querySelector('.website-content.terminal');
 		let title: any = document.querySelector('.title.terminal');
+		let line4: any = document.querySelector('.line4');
+		let line3Cursor: any = document.querySelector('.line3 .white-cursor');
+		let line4Text: any = document.querySelector('.line4 .terminal-text');
 		line3.addEventListener('animationend', () => {
 			setTimeout(() => {
+				line3Cursor.style.visibility = 'hidden';
+				line4.style.display = 'block';
 				animation.setAttribute('class', animation.getAttribute('class') + ' command-init');
 				title.setAttribute('class', title.getAttribute('class') + ' command-init');
+				line4Text.style.animationPlayState = 'running';
 			}, 200);
+		});
+		
+		let line5: any = document.querySelector('.line5');
+		let line5Text: any = document.querySelector('.line5 .terminal-text');
+		line4.addEventListener('animationend', () => {
+			setTimeout(() => {
+				line1.style.display = 'none';
+				line2.style.display = 'none';
+				line3.style.display = 'none';
+				line4.style.display = 'none';
+				line5.style.display = 'block';
+				line5Text.style.animationPlayState = 'running';
+			});
+		});
+
+		line5.addEventListener('animationend', () => {
+			setTimeout(() => {
+				let list = document.querySelectorAll('.command-init');
+				_.each(list, (el) => {
+					el.classList.remove('command-init');
+				});
+				animation.style.display = 'none';
+        let websiteContainer: any = document.querySelector('.website-container');
+        websiteContainer.style.visibility = 'visible';
+        let websiteContent: any = document.querySelector('.website-content.hidden');
+        websiteContent.style.visibility = 'visible';
+        title.style.visibility = 'visible';
+        let animationContainer: any = document.querySelector('.animation-initial');
+        animationContainer.classList.remove('animation-initial');
+			  websiteContent.classList.add('active');
+      }, 200);
 		});
 	}
 
