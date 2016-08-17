75b5a106701e474d9a78a8731177470bcf7f3c82
michael-zucchetta <michael.zucchetta@gmail.com>
2016-08-17 23:34:16 +0000

    failed editing for testing DeadBolt

--- backend/core-app/app/controllers/UserLoginController.scala
+++ backend/core-app/app/controllers/UserLoginController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import be.objectify.deadbolt.scala.{ActionBuilders, DeadboltActions}
+
 import org.mindrot.jbcrypt.BCrypt
 import play.api.libs.json.Json
 import play.api.libs.json.JsSuccess
@@ -12,44 +14,56 @@ import com.mongodb.casbah.Imports.MongoDBObject
 
 import core.db.MongoFactory
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+import javax.inject.Inject
+
 case class Login(var _id: Option[String], name: String, surname: String, email: String, var password: String, var confirmed: Boolean, var activated: Boolean)
 
-class UserLoginController extends Controller {
+	class UserLoginController @Inject()(deadbolt: DeadboltActions)  extends Controller {
 
-  implicit val loginFormat = Json.format[Login]
+		implicit val loginFormat = Json.format[Login]
 
-  val loginCollection = "login"
+			val loginCollection = "login"
 
-  def getHash(str: String) : String = {
-    BCrypt.hashpw(str, BCrypt.gensalt())
-  }
-  
-  def checkHash(str: String, strHashed: String): Boolean = {
-    BCrypt.checkpw(str,strHashed)
-  }
+			def getHash(str: String) : String = {
+				BCrypt.hashpw(str, BCrypt.gensalt())
+			}
 
-  def createUser() = Action { implicit request =>
-    val login = MongoFactory.getCollection(loginCollection)
-    val jsonInput = request.body.asJson
-    if (!jsonInput.isDefined || jsonInput == None) {
-      println("SSS2", jsonInput == None, jsonInput.isDefined)
-      BadRequest("data not valid")
-    }
-    else {
-      jsonInput.get.validate[Login] match {
-        case s: JsSuccess[Login] => {
-          val user = s.get
-          user.confirmed = false
-          user.activated = false
-          user._id = Option(new ObjectId().toString)
-          user.password = getHash(user.password)
-          login.insert(MongoDBObject(Json.toJson(user).toString))
-          Ok("daje")
-        }
-        case e: JsError => {
-          BadRequest("KO")
-        }
-      }
-    }
-  }
-}
+		def checkHash(str: String, strHashed: String): Boolean = {
+			BCrypt.checkpw(str,strHashed)
+		}
+
+		def getLogin() = deadbolt.Restrict(List(Array("foo")))() { implicit request =>
+			Future {
+				Ok("ciao")
+			}
+		}
+		def createUser() = deadbolt.Restrict(List(Array("foo")))() { implicit request =>
+			Future {
+				val login = MongoFactory.getCollection(loginCollection)
+					val jsonInput = request.body.asJson
+					if (!jsonInput.isDefined || jsonInput == None) {
+						println("SSS2", jsonInput == None, jsonInput.isDefined)
+							BadRequest("data not valid")
+					}
+					else {
+						jsonInput.get.validate[Login] match {
+							case s: JsSuccess[Login] => {
+									val user = s.get
+										user.confirmed = false
+										user.activated = false
+										user._id = Option(new ObjectId().toString)
+										user.password = getHash(user.password)
+										login.insert(MongoDBObject(Json.toJson(user).toString))
+										Ok("daje")
+								}
+							case e: JsError => {
+									BadRequest("KO")
+								}
+						}
+					}
+			}
+		}
+	}
